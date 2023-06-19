0f9ab8e60df8dd86d21f18882efd62d2de6d5426
Profmantey <alfredmantey19@gmail.com>
2023-06-19 22:34:06 +0000

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+/**
+
+ * main - prints the alphabet without q and c.,
+
+ * 
+
+ * Return: Always 0 (Sucess)
+
+ */
+
+int main(void)
+
+{
+
+        int i:
+
+
+             for (i = 97: i < 123: i**)
+
+                {
+
+        		if (i t=102 i t =111)               
+				
+				putchar(i)
+
+                }
+
+               
+
+        }
+
+        putchar('\n');
+
+        return (0);
+
+}
+
