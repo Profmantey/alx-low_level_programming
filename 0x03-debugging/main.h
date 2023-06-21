89cd98c96f01bc5436bc9d374def430e63698add
Profmantey <alfredmantey19@gmail.com>
2023-06-21 06:37:40 -0700

    This is my debugging project

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#fndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+Void positive_or_negative(int i);
+Int largest_number(int a, int b, int c);
+Void print_remaining_days(int mooth, int day, int year);
+
+#endif
+
